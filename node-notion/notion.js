d093293d9e037de53d5623b009f3a8eda7f1d432
Jan De Wilde <jan@jandewilde.com>
2023-12-22 23:45:35 -0500

    Separate ids for shrimp and fish database property keys

--- node-notion/notion.js
+++ node-notion/notion.js
@@ -6,7 +6,7 @@ const { writeJson } = require('./library.js');
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
-const n = {
+const fishKeys = {
   ammonia: 'CeXJ',
   nitrites: 'yJhy',
   nitrates: 'T%3Bhu',
@@ -25,7 +25,30 @@ const n = {
   fertilizer: 'yOKq',
   notes: 'title',
   date: 'GuZ%3E',
+  spongeClean: '%A%7BFF',
 };
+
+const shrimpKeys = {
+  ammonia: 'CeXJ',
+  nitrites: 'yJhy',
+  nitrates: 'T%3Bhu',
+  gh: '%7Do%3Bs',
+  chlorine: 'za_R',
+  kh: 'p~%3C%3D',
+  ph: 'Nc%3A%5C',
+  tds: '%7C~xd',
+  prime: '%3Cz~N',
+  waterChange: '%3C%5EmE',
+  stability: '%7BBBc',
+  co2: 'THso',
+  bakingSoda: 'JBgL',
+  replacedFilter: '',
+  crushedCoral: '',
+  fertilizer: 'zPDM',
+  notes: 'title',
+  date: 'GuZ%3E',
+  spongeClean: '%5DUZh',
+}
 
 // Use this function to log the properties of a database
 // Useful for finding the schema, ids of properties
@@ -36,17 +59,19 @@ async function getDatabase(id) {
   console.log(response);
 }
 
-async function getAquariumLogPages(id) {
+
+
+async function getAquariumLogPages(id, keys) {
   const notionPages = await notion.databases.query({
     database_id: id,
     sorts: [
       {
-        property: n.date,
+        property: keys.date,
         direction: 'descending',
       },
     ],
   });
-  return notionPages.results.map(fromNotionObject);
+  return notionPages.results.map(page => fromNotionObject(page, keys));
 }
 
 function notionPropertiesById(properties) {
@@ -56,38 +81,40 @@ function notionPropertiesById(properties) {
   }, {});
 }
 
-function fromNotionObject(notionPage) {
+function fromNotionObject(notionPage, keys) {
   // const id = notionPage.id;
   const propsById = notionPropertiesById(notionPage.properties);
   return {
     emoji: notionPage?.icon?.emoji ?? null,
-    date: propsById[n.date]?.date?.start ?? null,
-    title: propsById[n.notes]?.title[0]?.plain_text ?? null,
-    ammonia: propsById[n.ammonia]?.number ?? null,
-    nitrites: propsById[n.nitrites]?.number ?? null,
-    nitrates: propsById[n.nitrates]?.number ?? null,
-    gh: propsById[n.gh]?.number ?? null,
-    chlorine: propsById[n.chlorine]?.number ?? null,
-    kh: propsById[n.kh]?.number ?? null,
-    ph: propsById[n.ph]?.number ?? null,
-    tds: propsById[n.tds]?.number ?? null,
-    prime: propsById[n.prime]?.number ?? null,
-    waterChange: propsById[n.waterChange]?.number ?? null,
-    stability: propsById[n.stability]?.checkbox ?? null,
-    co2: propsById[n.co2]?.checkbox ?? null,
-    bakingSoda: propsById[n.bakingSoda]?.checkbox ?? null,
-    replacedFilter: propsById[n.replacedFilter]?.checkbox ?? null,
-    crushedCoral: propsById[n.crushedCoral]?.checkbox ?? null,
-    fertilizer: propsById[n.fertilizer]?.checkbox ?? null,
+    date: propsById[keys.date]?.date?.start ?? null,
+    title: propsById[keys.notes]?.title[0]?.plain_text ?? null,
+    ammonia: propsById[keys.ammonia]?.number ?? null,
+    nitrites: propsById[keys.nitrites]?.number ?? null,
+    nitrates: propsById[keys.nitrates]?.number ?? null,
+    gh: propsById[keys.gh]?.number ?? null,
+    chlorine: propsById[keys.chlorine]?.number ?? null,
+    kh: propsById[keys.kh]?.number ?? null,
+    ph: propsById[keys.ph]?.number ?? null,
+    tds: propsById[keys.tds]?.number ?? null,
+    prime: propsById[keys.prime]?.number ?? null,
+    waterChange: propsById[keys.waterChange]?.number ?? null,
+    stability: propsById[keys.stability]?.checkbox ?? null,
+    co2: propsById[keys.co2]?.checkbox ?? null,
+    bakingSoda: propsById[keys.bakingSoda]?.checkbox ?? null,
+    replacedFilter: propsById[keys.replacedFilter]?.checkbox ?? null,
+    crushedCoral: propsById[keys.crushedCoral]?.checkbox ?? null,
+    fertilizer: propsById[keys.fertilizer]?.checkbox ?? null,
   };
 }
 
 (async () => {
-  const fishData = await getAquariumLogPages(process.env.NOTION_FISH_DB_ID);
+  // getDatabase(process.env.NOTION_FISH_DB_ID);
+
+  const fishData = await getAquariumLogPages(process.env.NOTION_FISH_DB_ID, fishKeys);
   const fishPathToJson = path.join(__dirname, '..', 'src', '_data', 'fishData.json');
   writeJson(fishPathToJson, fishData);
 
-  const shrimpData = await getAquariumLogPages(process.env.NOTION_SHRIMP_DB_ID);
+  const shrimpData = await getAquariumLogPages(process.env.NOTION_SHRIMP_DB_ID, shrimpKeys);
   const shrimpPathToJson = path.join(__dirname, '..', 'src', '_data', 'shrimpData.json');
   writeJson(shrimpPathToJson, shrimpData);
 })();
