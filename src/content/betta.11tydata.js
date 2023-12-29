ff599cd2d623f78b7749db118a1c696f22502354
Jan De Wilde <jan@jandewilde.com>
2023-12-29 18:45:48 -0500

    Additional refactoring

--- src/content/betta.11tydata.js
+++ src/content/betta.11tydata.js
@@ -1,24 +1,5 @@
 const { DateTime } = require('luxon');
 
-const now = DateTime.local();
-const currentMonth = now.month;
-const yearCurrentMonth = now.year;
-
-function getPreviousMonthAndYear(currentMonth, yearCurrentMonth) {
-  const previousDate = DateTime.local(yearCurrentMonth, currentMonth).minus({
-    months: 1,
-  });
-  return {
-    previousMonth: previousDate.month,
-    yearPreviousMonth: previousDate.year,
-  };
-}
-
-const { previousMonth, yearPreviousMonth } = getPreviousMonthAndYear(
-  currentMonth,
-  yearCurrentMonth
-);
-
 const FISH_DATA_PROPERTIES = [
   'prime',
   'waterChange',
@@ -30,12 +11,36 @@ const FISH_DATA_PROPERTIES = [
   'fertilizer',
 ];
 
+const FIRST_DAY_OF_WEEK = 1;
+const LAST_DAY_OF_WEEK = 7;
+
+const now = DateTime.local();
+const currentMonth = now.month;
+const yearCurrentMonth = now.year;
+const { previousMonth, yearPreviousMonth } = getPreviousMonthAndYear(
+  currentMonth,
+  yearCurrentMonth
+);
+
+function getPreviousMonthAndYear(currentMonth, yearCurrentMonth) {
+  const previousDate = DateTime.local(yearCurrentMonth, currentMonth).minus({
+    months: 1,
+  });
+  return {
+    previousMonth: previousDate.month,
+    yearPreviousMonth: previousDate.year,
+  };
+}
+
+
 function getFishDataForDay(fishData, day) {
   const fishDataForDay = fishData.find((fish) => fish.date === day.isoString);
-  return FISH_DATA_PROPERTIES.reduce((acc, property) => {
-    acc[property] = fishDataForDay?.[property] ?? null;
-    return acc;
-  }, {});
+  return Object.fromEntries(
+    FISH_DATA_PROPERTIES.map((property) => [
+      property,
+      fishDataForDay?.[property] ?? null,
+    ])
+  );
 }
 
 function addFishDataToCalendar(calendarData, fishData) {
@@ -47,15 +52,17 @@ function addFishDataToCalendar(calendarData, fishData) {
 
 function getDaysInMonth(month, year) {
   const days = [];
-  const daysInMonth = DateTime.local(year, month).daysInMonth;
+  const monthDate = DateTime.local(year, month);
+  const daysInMonth = monthDate.daysInMonth;
+  const monthHumanReadable = monthDate.monthLong;
   let daysToSkip, daysToAppend;
   for (let i = 1; i <= daysInMonth; i++) {
     const date = DateTime.local(year, month, i);
     if (i === 1) {
-      daysToSkip = date.weekday - 1;
+      daysToSkip = date.weekday - FIRST_DAY_OF_WEEK;
     }
     if (i === daysInMonth) {
-      daysToAppend = 7 - date.weekday;
+      daysToAppend = LAST_DAY_OF_WEEK - date.weekday;
     }
     const day = {
       day: String(date.day).padStart(2, '0'),
@@ -64,24 +71,24 @@ function getDaysInMonth(month, year) {
     };
     days.push(day);
   }
-  return { month, year, days, daysToSkip, daysToAppend };
+  return { monthHumanReadable, year, days, daysToSkip, daysToAppend };
 }
 
 module.exports = {
   eleventyComputed: {
     calendarData(data) {
       const fishData = data.fishData;
-      const currentMonthDays = getDaysInMonth(currentMonth, yearCurrentMonth);
+      const daysInCurrentMonth = getDaysInMonth(currentMonth, yearCurrentMonth);
       const currentMonthCalendarData = addFishDataToCalendar(
-        currentMonthDays,
+        daysInCurrentMonth,
         fishData
       );
-      const previousMonthDays = getDaysInMonth(
+      const daysInPreviousMonth = getDaysInMonth(
         previousMonth,
         yearPreviousMonth
       );
       const previousMonthCalendarData = addFishDataToCalendar(
-        previousMonthDays,
+        daysInPreviousMonth,
         fishData
       );
 
