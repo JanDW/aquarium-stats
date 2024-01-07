bc5ff9e95678cbd3fd6a3c1e29d3eacd5da3143b
Jan De Wilde <jan@jandewilde.com>
2024-01-07 14:55:25 -0500

    Use tailwind for layout

--- src/content/content.11tydata.js
+++ src/content/content.11tydata.js
@@ -1,5 +1,5 @@
 module.exports = {
-  bodyClasses: 'home bg-slate-900 text-white font-inter',
+  bodyClasses: 'home bg-slate-900 text-white font-inter mx-2',
   mainClasses: 'main-grid',
   eleventyComputed: {
     socialImg: (data) => {
