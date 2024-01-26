fc04a57bf2027e0092e2ffe78c0684c5d226c4ce
Jan De Wilde <jan@jandewilde.com>
2024-01-25 19:55:20 -0500

    Add links to notion source databases

--- src/_data/processEnv.js
+++ src/_data/processEnv.js
@@ -2,5 +2,8 @@ require('dotenv').config();
 
 module.exports = () => ({
   environment: process.env.ELEVENTY_ENV,
+  twoGallonId: process.env.NOTION_TWO_GALLON_DB_ID,
+  fiveGallonId: process.env.NOTION_FIVE_GALLON_DB_ID,
+  tenGallonId: process.env.NOTION_TEN_GALLON_DB_ID,
 });
  
