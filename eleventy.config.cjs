571f2f39f0ee21f04a3382671133aa15254f55b1
Jan De Wilde <jan@jandewilde.com>
2024-02-06 14:48:48 -0500

    recentData macro formatting tweaks

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,8 +1,9 @@
-const markdownIt = require('markdown-it');
-const { DateTime } = require('luxon');
-const yaml = require('js-yaml');
-const bundlerPlugin = require('@11ty/eleventy-plugin-bundle');
-const prettier = require('./src/transforms/prettier.js');
+const markdownIt = require("markdown-it");
+const { DateTime } = require("luxon");
+const yaml = require("js-yaml");
+const bundlerPlugin = require("@11ty/eleventy-plugin-bundle");
+const prettier = require("./src/transforms/prettier.js");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
   const mdOptions = {
@@ -14,22 +15,22 @@ module.exports = function (eleventyConfig) {
 
   const markdownLib = markdownIt(mdOptions);
 
-  eleventyConfig.setLibrary('md', markdownLib);
+  eleventyConfig.setLibrary("md", markdownLib);
   eleventyConfig.setUseGitIgnore(true);
 
   // Passthroughs
   const stuffToCopy = [
-    'src/assets/img',
-    { 'src/assets/siteroot': '/' },
+    "src/assets/img",
+    { "src/assets/siteroot": "/" },
     {
-      'node_modules/chart.js/dist/chart.umd.js': 'assets/scripts/chart.umd.js',
+      "node_modules/chart.js/dist/chart.umd.js": "assets/scripts/chart.umd.js",
     },
     {
-      'node_modules/@11ty/is-land/is-land.js': 'assets/scripts/is-land.js',
+      "node_modules/@11ty/is-land/is-land.js": "assets/scripts/is-land.js",
     },
     {
-      'node_modules/@zachleat/snow-fall/snow-fall.js':
-        'assets/scripts/snow-fall.js',
+      "node_modules/@zachleat/snow-fall/snow-fall.js":
+        "assets/scripts/snow-fall.js",
     },
   ];
 
@@ -38,20 +39,36 @@ module.exports = function (eleventyConfig) {
   });
 
   // Shortcodes
-  eleventyConfig.addPairedShortcode('markdown', (content) => {
+  eleventyConfig.addPairedShortcode("markdown", (content) => {
     return markdownLib.render(content);
   });
 
+  const imageShortcode = async (
+    src,
+    alt,
+    className = undefined,
+    widths = [640, 768, 1024, 1280, 1536],
+    formats = ["webp", "jpeg"],
+    sizes = "100vw",
+  ) => {
+    // we'll fill this in shortly
+  };
+
+  eleventyConfig.addShortcode("image", imageShortcode);
+
+  module.exports = (eleventyConfig) => {
+    eleventyConfig.addShortcode("image", imageShortcode);
+  };
 
   // Plugins
   eleventyConfig.addPlugin(bundlerPlugin);
 
-  eleventyConfig.addFilter('toHeading', function (value) {
+  eleventyConfig.addFilter("toHeading", function (value) {
     // Split the string on camelCase boundaries, then capitalize the first letter of each word
     let result = value
       .split(/(?=[A-Z])/)
       .map((word) => word.toLowerCase())
-      .join(' ');
+      .join(" ");
 
     // Ensure the first letter of the result is capitalized
     result = result.charAt(0).toUpperCase() + result.slice(1);
@@ -59,38 +76,38 @@ module.exports = function (eleventyConfig) {
     return result;
   });
 
-  eleventyConfig.addFilter('readableDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
-      'MMMM d, yyyy'
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "MMMM d, yyyy",
     );
   });
 
   // Filter that converts 2023-12-26 to Dec 26
-  eleventyConfig.addFilter('shortDate', (dateStr) => {
-    return DateTime.fromISO(dateStr, { zone: 'utc' }).toFormat('MMM d');
+  eleventyConfig.addFilter("shortDate", (dateStr) => {
+    return DateTime.fromISO(dateStr, { zone: "utc" }).toFormat("MMM dd");
   });
 
-  eleventyConfig.addNunjucksFilter('getData', function (value) {
+  eleventyConfig.addNunjucksFilter("getData", function (value) {
     return this.ctx[value];
   });
 
   // Add Custom Data Extensions YAML
-  eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
 
   // Transforms
-  eleventyConfig.addTransform('prettier', prettier);
+  eleventyConfig.addTransform("prettier", prettier);
 
   return {
     dir: {
       // ⚠️ These values are both relative to your input directory.
-      includes: '_includes',
-      layouts: '_layouts',
-      input: 'src',
-      output: 'www',
+      includes: "_includes",
+      layouts: "_layouts",
+      input: "src",
+      output: "www",
     },
-    templateFormats: ['md', 'html', 'njk'],
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
+    templateFormats: ["md", "html", "njk"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
   };
 };
