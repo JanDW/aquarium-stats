5a6a4dd793e59b662ef9ac4175f9cd419e3eb2bb
Jan De Wilde <jan@jandewilde.com>
2024-02-28 21:51:33 -0500

    Prettier config

--- .prettier.config.js
+++ .prettier.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ["prettier-plugin-tailwindcss"],
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  semi: true,
+  arrowParens: "always",
+  printWidth: 80,
+  bracketSpacing: true,
 };
